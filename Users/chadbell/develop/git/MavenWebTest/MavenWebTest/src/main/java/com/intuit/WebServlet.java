2f863d3c1e4ce77d4a717611f43ebf788c58e528
Chad Bell <chad_bell@intuit.com>
2013-04-01 16:36:04 -0700

    now a 13

--- Users/chadbell/develop/git/MavenWebTest/MavenWebTest/src/main/java/com/intuit/WebServlet.java
+++ Users/chadbell/develop/git/MavenWebTest/MavenWebTest/src/main/java/com/intuit/WebServlet.java
@@ -11,6 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 public class WebServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
-		out.println("This is a test 12");
+		out.println("This is a test 13");
 	}
 }
